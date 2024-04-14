d39f4fac94d70438b4e78b82d2adb000f0546848
williamorim <thewilliam89@gmail.com>
2024-04-14 00:12:43 -0300

    pegando legendas

--- data-raw/pegar_legenda_yt/pegar_legendas.R
+++ data-raw/pegar_legenda_yt/pegar_legendas.R
@@ -11,7 +11,7 @@ res_channel <- tuber::yt_search(
   type = "channel"
 )
 
-saveRDS(res_channel$channelId, "data-raw/channel_id.rds")
+saveRDS(res_channel$channelId, "data-raw/pegar_legenda_yt/channel_id.rds")
 
 
 # Get videos --------------------------------------------------------------
@@ -21,7 +21,7 @@ tuber::yt_oauth(
   app_secret = Sys.getenv("gcp_app_secret")
 )
 
-channel_id <- readRDS("data-raw/channel_id.rds")
+channel_id <- readRDS("data-raw/pegar_legenda_yt/channel_id.rds")
 
 res_videos_pt1 <- tuber::yt_search(
   term = "Naruhodo",
@@ -68,7 +68,7 @@ tab_videos <- rbind(
     podcast_num = as.numeric(stringr::str_extract(video_name, "#[0-9]+"))
   )
 
-saveRDS(tab_videos, "data-raw/tab_videos_v1.rds")
+saveRDS(tab_videos, "data-raw/pegar_legenda_yt/tab_videos_v1.rds")
 
 
 # Get missing videos ------------------------------------------------------
@@ -78,8 +78,8 @@ tuber::yt_oauth(
   app_secret = Sys.getenv("gcp_app_secret")
 )
 
-channel_id <- readRDS("data-raw/channel_id.rds")
-tab_videos <- readRDS("data-raw/tab_videos_v1.rds")
+channel_id <- readRDS("data-raw/pegar_legenda_yt/channel_id.rds")
+tab_videos <- readRDS("data-raw/pegar_legenda_yt/tab_videos_v1.rds")
 
 total_episodes <- 1:414
 missing_episodes <- total_episodes[!total_episodes %in% tab_videos$podcast_num]
@@ -132,54 +132,95 @@ tab_videos_completa <- tab_missing |>
 
 total_episodes[!total_episodes %in% tab_videos_completa$podcast_num]
 
-saveRDS(tab_videos_completa, "data-raw/tab_videos_completa.rds")
+saveRDS(tab_videos_completa, "data-raw/pegar_legenda_yt/tab_videos_completa.rds")
+
+# Pegar url das legendas  -------------------------------------------------
+
+tab_videos_completa <- readRDS("data-raw/pegar_legenda_yt/tab_videos_completa.rds")
+
+pegar_url_legenda <- function(video_id) {
+
+  Sys.sleep(2)
+
+  res <- httr2::request("https://www.youtube.com/watch") |>
+    httr2::req_url_query(v = video_id) |>
+    httr2::req_perform()
+
+  res |>
+    httr2::resp_body_string() |>
+    stringr::str_extract('"https://www.youtube.com/api/timedtext(.+?)"') |>
+    stringr::str_remove_all('"') |>
+    stringi::stri_unescape_unicode()
+}
+
+tab <- tab_videos_completa |>
+  dplyr::mutate(
+    legenda_url = purrr::map_chr(
+      video_id,
+      pegar_url_legenda,
+      .progress = TRUE
+    )
+  )
+
+saveRDS(tab, "data-raw/pegar_legenda_yt/tab_videos_com_legenda.rds")
+
+sem_url <- tab |>
+  dplyr::filter(is.na(legenda_url)) |>
+  dplyr::pull(video_id)
+
 
 # Pegar legenda -----------------------------------------------------------
 
-tuber::yt_oauth(
-  app_id = Sys.getenv("gcp_app_id"),
-  app_secret = Sys.getenv("gcp_app_secret")
-)
+tab <- readRDS("data-raw/pegar_legenda_yt/tab_videos_com_legenda.rds")
 
-tab_videos_completa <- readRDS("data-raw/tab_videos_completa.rds")
+pegar_legenda <- function(url) {
 
-safe_list_caption_tracks <- purrr::possibly(
-  tuber::list_caption_tracks,
-  otherwise = NULL
-)
-
-pegar_id_legenda <- function(video_id) {
-
-  res <- safe_list_caption_tracks(
-    video_id = video_id,
-    language = "pt"
-  )
-
-  Sys.sleep(1)
-
-  if (is.null(res)) {
-    return(NA)
-  } else if (nrow(res) == 0) {
-    return(NA)
-  } else {
-    return(res$id)
+  if (is.na(url)) {
+    return(NA_character_)
   }
 
+  res_legenda <- httr2::request(url) |>
+    httr2::req_perform()
+
+  res_legenda |>
+    httr2::resp_body_xml() |>
+    xml2::xml_text()
 }
 
-n <- 1
-legenda_id <- c()
 
-for (video_id in tab_videos_completa$video_id[n:(n+49)]) {
-  legenda_id <- c(legenda_id, pegar_id_legenda(video_id))
-}
+tab_legenda <- tab |>
+  dplyr::mutate(
+    legenda = purrr::map_chr(
+      legenda_url,
+      pegar_legenda,
+      .progress = TRUE
+    )
+  )
 
-tab_videos_completa$legenda_track_id[n:(n+49)] <- legenda_id
+tab_legenda |> dplyr::filter(is.na(legenda))
 
-saveRDS(tab_videos_completa, "data-raw/tab_videos_completa.rds")
+tab_legenda |>
+  dplyr::select(
+    video_name,
+    video_id,
+    video_pub_date,
+    podcast_tipo,
+    legenda_url,
+    legenda_texto = legenda
+  ) |>
+  saveRDS("data-raw/dados_nahurodo.rds")
 
-teste <- tuber::get_captions(
-  id = tab_videos_completa$legenda_track_id[1],
-  lang = "pt"
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
 

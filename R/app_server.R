17ce9abfd0854e492a929e85647b8b720ba1c241
williamorim <thewilliam89@gmail.com>
2024-04-16 23:49:52 -0300

    estrutra do golem

---
+++ R/app_server.R
@@ -0,0 +1,14 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_server <- function(input, output, session) {
+  # Your application server logic
+
+  dados <- readr::read_csv(
+    app_sys("app/dados_nahurodo.csv")
+  )
+
+}

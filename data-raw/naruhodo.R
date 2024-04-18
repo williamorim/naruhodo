e67f48ed2239e0da8216f612f62cf3b0db4c8f1d
williamorim <thewilliam89@gmail.com>
2024-04-18 16:30:05 -0300

    legendas tratadas

--- data-raw/naruhodo.R
+++ data-raw/naruhodo.R
@@ -4,6 +4,11 @@ tab_capas <- readRDS("data-raw/pegar_capas/tab_capas.rds")
 
 
 tab_legenda |>
+  dplyr::mutate(
+    legenda_tratada = legenda_texto |>
+      tolower() |>
+      stringi::stri_trans_general("Latin-ASCII")
+  ) |>
   dplyr::left_join(
     tab_capas,
     by = c("podcast_tipo", "podcast_num")
